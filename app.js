1f3ef014d3340ebef19b1b3c29ee61dfc42c332d
conorbailey90 <conbailey90@gmail.com>
2020-08-30 13:21:25 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+let modal = document.querySelector('.modal');
+let noteForm = document.querySelector('.note-form');
+let noteTable = document.querySelector('.note-table')
+let cancel = document.querySelector('.cancel-btn')
+
+let noteList = [];
+let noteDeleteButtons;
+
+noteForm.addEventListener('submit', (e)=>{
+    addNote(e);
+});
+
+function getDeletButtons(){
+    noteDeleteButtons = Array.from(document.querySelectorAll('.delete-item'));
+
+    noteDeleteButtons.forEach(button =>{
+        let noteTitle = button.previousSibling.previousSibling.innerText
+        button.addEventListener('click', ()=>{
+            deleteNote(noteTitle);
+        }
+    )
+})
+}
+
+function addNote(e){
+    e.preventDefault();
+    let newNote = {};
+
+    let title = document.querySelector('.title');
+    let note = document.querySelector('.note');
+
+    if (title.value == '' || note.value == ''){
+        return alert('Please fill in both fields');
+    }else{
+        newNote.title = title.value;
+        newNote.note = note.value;
+    }
+    title.value = '';
+    note.value = '';
+
+    noteList.push(newNote)
+    appendNotes();
+    cancel.click();
+}
+
+function appendNotes(){
+    
+    let notes = Array.from(document.querySelectorAll('.noteItem'));
+    if (notes.length > 0){ 
+        notes.forEach(note =>{
+            note.remove();
+        })
+    }
+    noteList.map(note => {
+
+        // Create cells
+        let tr = document.createElement('tr');
+        tr.classList = 'noteItem';
+        let tdTitle = document.createElement('td');
+        tdTitle.innerText = note.title;
+        let tdNote = document.createElement('td');
+        tdNote.innerText = note.note;
+        let tdDelete = document.createElement('td');
+        tdDelete.innerHTML = '&times'
+        tdDelete.classList.add('delete-item')
+
+        // Append cells to table row
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdNote);
+        tr.appendChild(tdDelete);
+
+        //Append tr to table
+        noteTable.appendChild(tr);
+        getDeletButtons();
+    })
+}
+
+function deleteNote(noteTitle){
+    for(let i = 0; i < noteList.length; i++){
+        if(noteList[i].title == noteTitle){
+            noteList.splice(i, 1);
+        }
+    }
+    appendNotes();
+}
